73850a5dad6eb17414cc9c17c2eb36d20a4a10f7
Kirby <kirbyjallen@gmail.com>
2023-08-11 16:37:15 -0500

    Account for negative error

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,16 @@
 const sumAll = function(x, y) {
-    let x=0
+    if (x < 0) return "ERROR";
+    
+}
+   
+   
+    /* let x=0
     let y=0
     while (x<10) {
         x++;
-        y += x;
-    }
-};
+        y += x; 
+    } */
+
 
 // Do not edit below this line
 module.exports = sumAll;
