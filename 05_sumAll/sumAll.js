a99514abf7ea961b004ac8ae1b675e0773b7f860
Kirby <kirbyjallen@gmail.com>
2023-08-11 16:21:32 -0500

    test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let x=0
+    let y=0
+    while (x<10) {
+        x++;
+        y += x;
+    }
 };
 
 // Do not edit below this line
