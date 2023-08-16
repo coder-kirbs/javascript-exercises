8a240005b1b5339be08be24fed88e952ba37a944
Kirby <kirbyjallen@gmail.com>
2023-08-16 14:30:17 -0500

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function (min, max) {
     if (min > max) [min, max] = [max, min];
-    if (min < 0 || max < 0) return "ERROR";
+    if (min < 0 || max  < 0) return "ERROR";
     if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
    
