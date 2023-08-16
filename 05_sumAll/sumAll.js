a92c67414700c01aa01fc70fb6fec96fde46f835
Kirby <kirbyjallen@gmail.com>
2023-08-16 14:11:55 -0500

    Complete function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function (min, max) {
-    if (min<max) [min, max] = [max, min];
-    if (min<0 || max<0) return "ERROR";
-}
+    if (min > max) [min, max] = [max, min];
+    if (min < 0 || max<0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
    
     /* let x=0
@@ -11,6 +11,12 @@ const sumAll = function (min, max) {
         y += x; 
     } */
 
+    let sum = 0;
+    for (let i = min; i <= min; i++) {
+        sum += i;
+    }
+    return sum;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
