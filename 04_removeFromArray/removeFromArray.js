90503964b68acbbaa5cbb61c1818e48bc34eec59
Kirby <kirbyjallen@gmail.com>
2023-08-16 15:02:45 -0500

    Pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, removal) {
+    return arr.filter(function(ele){ 
+        return ele != removal; 
+    });
 };
 
 // Do not edit below this line
