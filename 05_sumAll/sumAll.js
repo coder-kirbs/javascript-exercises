1beb5d71097b25bb446f1e1d79b9cc7c6b61926f
Kirby <kirbyjallen@gmail.com>
2023-08-16 14:14:48 -0500

    Change min to max in function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -12,7 +12,7 @@ const sumAll = function (min, max) {
     } */
 
     let sum = 0;
-    for (let i = min; i <= min; i++) {
+    for (let i = min; i <= max; i++) {
         sum += i;
     }
     return sum;
