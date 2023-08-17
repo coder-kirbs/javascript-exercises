4d4bce61105721deb5230f122e162d0574adb5f9
Kirby <kirbyjallen@gmail.com>
2023-08-17 10:23:24 -0500

    Create inverse of function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,10 @@
 const leapYears = function(year) {
-    if (year % 4===0 && year % 100===0) {
-        return "true"
+    if (year % 4!==0 && year % 100!==0 && year % 400!==0) {
+        return false
     } else if (year % 4===0) {
-        return "true"
+        return true
     } else {
-        return "false"
+        return false
     }
 };
 
