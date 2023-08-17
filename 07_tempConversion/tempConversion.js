f0909b77f6a75a694ee6746d6ab11a95f1278960
Kirby <kirbyjallen@gmail.com>
2023-08-17 10:53:58 -0500

    Add convertToFarenheit function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function(fare) {
-  let sum = (fare - 32) * (5/9)
-  return sum
+  let fSum = (fare - 32) * (5/9)
+  return fSum
 };
 
 const convertToFahrenheit = function(cels) {
-};
+  let cSum = 9*(cels)/5 + 32
+  return cSum
+}; 
 
 // Do not edit below this line
 module.exports = {
