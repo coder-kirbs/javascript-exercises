dcf9716217f866693f5acd0ddf4584c26332308f
Kirby <kirbyjallen@gmail.com>
2023-08-17 10:40:12 -0500

    Final

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -9,9 +9,5 @@ const leapYears = function(year) {
         return false
 };
 
-// if year /100 && /4 then leap_year
-// if year /4, then =leap_year
-
-
 // Do not edit below this line
 module.exports = leapYears;
