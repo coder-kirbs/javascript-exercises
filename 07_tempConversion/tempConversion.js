dffc30571b1c761cff72f41d5169815d88e213b5
Kirby <kirbyjallen@gmail.com>
2023-08-17 13:18:34 -0500

    Use Math.round to approximate

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fare) {
   let fSum = (fare - 32) * (5/9)
-  return fSum
+  return Math.round(fSum*10)/10
 };
 
 const convertToFahrenheit = function(cels) {
   let cSum = 9*(cels)/5 + 32
-  return cSum
+  return Math.round(cSum*10)/10
 }; 
 
 // Do not edit below this line
