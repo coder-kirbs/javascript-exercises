1d9ce9db5e1a8b7b1fb4a357abe8bf990c6618a7
Kirby <kirbyjallen@gmail.com>
2023-09-20 11:49:13 -0500

    Chain functions together for lowerString and backwards variables

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,9 @@
 const palindromes = function (string) {
     let re = /[^A-Za-z0-9]/g;
     let lowerString = string.toLowerCase().replace(re, '');
-    let backwards = lowerString.split('').reverse().join();
-    return lowerString===backwards;
+    let backwards = lowerString.split('').reverse().join('');
+    return lowerString === backwards;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
