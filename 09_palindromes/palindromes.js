a982923e5b9b45c19d62e5bcfb7d456eedd063e0
Kirby <kirbyjallen@gmail.com>
2023-09-20 11:37:48 -0500

    Replace if statement with boolean

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,8 @@
 const palindromes = function (string) {
     let re = /[^A-Za-z0-9]/g;
     let lowerString = string.toLowerCase().replace(re, '');
-    let backwards = string.reverse().replace(re, '');
-    
-    
-    if (string.toLowerCase()===string.reverse()) {
-        
-    } else {
-        
-    }
-    
-   
-};
+    let backwards = lowerString.split('').reverse().join();
+    return lowerString===backwards;
 
 // Do not edit below this line
 module.exports = palindromes;
