f5ac6699cf69d4cc4f5d47218ab553138b2b2965
Kirby <kirbyjallen@gmail.com>
2023-09-20 11:19:19 -0500

    Create first three variables

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let re = /[^A-Za-z0-9]/g;
+    let lowerString = string.toLowerCase().replace(re, '');
+    let backwards = string.reverse().replace(re, '');
+    
+    
+    if (string.toLowerCase()===string.reverse()) {
+        
+    } else {
+        
+    }
+    
+   
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// if string = reverse string, then palindrome
