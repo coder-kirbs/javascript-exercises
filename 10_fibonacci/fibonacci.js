206ecb04b130fd6b463b6c46853216b65b2fdd3f
Kirby <kirbyjallen@gmail.com>
2023-09-21 10:33:12 -0500

    Write code for fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,15 @@
 const fibonacci = function(n) {
 let num = Number(n); 
-if (num <= 0) {
+if (num < 0) {
     return "OOPS";
-} else {
+} else if (num === 0) {
+    return 0;    
+} else if (num <=2) {
     return 1;
-};
+} else {
+    let fib = fibonacci(num-1)+fibonacci(num-2)
+    return fib;
+}
 };
 
 // Do not edit below this line
