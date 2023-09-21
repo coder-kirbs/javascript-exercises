9d5f7e0dfd6d49a1d52d73b55bf3b69ffa989c5e
Kirby <kirbyjallen@gmail.com>
2023-09-21 09:59:54 -0500

    Input pseudocode

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+/* if input is 2 or less, return 1
+convert strings to numbers
+f(0) = f(n-1)+f(n-2)
+*/
