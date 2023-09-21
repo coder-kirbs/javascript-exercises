2f5e3e36e676b260dc1487c3440a30bbd8a3ac6b
Kirby <kirbyjallen@gmail.com>
2023-09-21 12:47:30 -0500

    Create function to find age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(personA, personB){
+        const lastPerson = personA.yearOfDeath-personA.yearOfBirth;
+        const nextPerson = personB.yearOfDeath-personB.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    console.log (oldest.slice(0,1)); 
 };
+
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
+
+//create table sorting by age
+//return oldest for each/first item in new array
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
